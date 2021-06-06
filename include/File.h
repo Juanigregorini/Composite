da425db9cb9774ff2f482103e84f7f321fedfea3
Juan Ignacio Gregorini <juanigregorini23@gmail.com>
2021-06-06 16:38:42 -0300

    actividad edi3

---
+++ include/File.h
@@ -0,0 +1,8 @@
+#ifndef FILE_H_INCLUDED
+#define FILE_H_INCLUDED
+#include "../src/File.cpp"
+
+void list(int depth){};
+
+
+#endif // FILE_H_INCLUDED
---
+++ include/Folder.h
@@ -0,0 +1,9 @@
+#ifndef FOLDER_H_INCLUDED
+#define FOLDER_H_INCLUDED
+#include "../src/Folder.cpp"
+
+    void list(int depth){
+    };
+
+
+#endif // FOLDER_H_INCLUDED
